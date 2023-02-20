c1ada10017afc3bada93e93afa492fc6bb01f488
Maruf360 <sayedalmaruf3@gmail.com>
2023-02-21 01:16:38 +0600

    default JS added

--- JS/DOM.js
+++ JS/DOM.js
@@ -1,13 +1,30 @@
-document.getElementById('btn-bg').addEventListener('click', function(){
+document.getElementById('btn-bg').addEventListener('click', function () {
     const friends = document.getElementsByClassName('friend');
-    for (const friend of friends){
-        friend.style.backgroundColor='lightBlue';
+    for (const friend of friends) {
+        friend.style.backgroundColor = 'lightBlue';
     }
-})
+
+
+
+
+});
+
 
 // center-the-third button
+document.getElementById('center-the-third').addEventListener('click', function () {
 
-document.getElementById('center-the-third').addEventListener('click', function(){
-    const thirdbutton=Document.getElementById('center-the-third');
-    thirdbutton.style.backgroundColor='blue';
+    const third = document.getElementById('third-friend');
+    third.style.textAlign = 'center';
+})
+
+// add new fried=nd button
+document.getElementById('add-friend').addEventListener('click', function () {
+    const friendsContainer = document.getElementById('friends');
+    const friend =document.createElement('div');
+    friend.classList.add('friend');
+    friend.innerHTML=`
+    <h3 class="friend-name"> New friend</h3>
+    <p>something new added </p>
+    `
+    friendsContainer.appendChild(friend);
 })
---
+++ JS/default.js
@@ -0,0 +1,7 @@
+function add(first,second = 0){
+const total=first+second;
+return total;
+}
+
+const result=add (12);
+console.log(result)
